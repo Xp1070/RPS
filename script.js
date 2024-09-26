575d91de4c775c7158403b1d3ea688a33e4e8126
Xp_107 <maciejgr.07@gmail.com>
2024-09-26 11:12:22 +0200

    1st version
    You can pick and our amzing randomizer AI can pick

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-//console.log("Hello world!");
-
-let Tool = function(name, weight, sound)
+let Object = function(name, strength, weakness)
 {
     this.name = name;
-    this.weight = weight;
-    this.sound = sound;
-    this.doSomething = function()
-    {
-        console.log("I am " + this.name + " and I can do " + this.sound + ".");
-    }
-    this.toString = function()
-    {
-        return("I am " + this.name + " my weight is " + this.weight + " and I can do " + this.sound + ".");
-    }
+    this.strength = strength;
+    this.weakness = weakness;
 }
-let drill = new Tool("Ultra-Drill 3000", "5kg", "vroom");
-let hammer = new Tool("Mjolnir", "3kg", "bonk bonk");
-let nailgun = new Tool("Nailgun", "2kg", "pew pew");
-let saw = new Tool("Shredder", "1kg", "wrzzzz wrzz");
-let screwdriver = new Tool("LTT Screwdriver","100g","ckrr");
-let sledgehammer= new Tool("Panzerabwehrhammer1050","6kg","BONK!");
-let toolbox = [drill, hammer, nailgun, saw, screwdriver, sledgehammer];
-//for(let i = 0; i < toolbox.length; i++)
-//{
-//    document.write(toolbox[i].toString() + "<br>");
-//}
-function randomize()
+let rock = new Object("rock", "scissors", "paper");
+let paper = new Object("paper", "rock", "scissors");
+let scissors = new Object("scissors", "paper", "rock");
+let object;
+let AIobjectId;
+let enemyObject;
+
+let pencilCase = [rock, paper, scissors];
+function pick(id)
 {
-    alert("Wygenerowano narzędzie!");
-    let index = Math.floor(Math.random() * toolbox.length);
-    //document.write("Wylosowane narzędzie: " + "<br>");
-    //document.write(toolbox[index].toString());
-    let p = document.getElementById("randomTool");
-    p.innerHTML = toolbox[index].toString()
+    numberId = parseInt(id);
+    object = pencilCase[numberId].name;
+    console.log(object);
+}
+function enemyTurn()
+{
+    ourAmazingRandomizerAI();
+    enemyObject = pencilCase[AIobjectId].name;
+    console.log(enemyObject);
+}
+function ourAmazingRandomizerAI()
+{
+    AIobjectId = Math.floor(Math.random() * 3);
 }
