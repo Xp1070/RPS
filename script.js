06b37bbb7edd41ee096ab80c0994143bf7d396b8
Xp_107 <maciejgr.07@gmail.com>
2024-09-27 17:50:59 +0200

    2nd version
    Comic sans is now working, we have results of games and everything is moved from the console to the website.
    Life is great :)

--- script.js
+++ script.js
@@ -10,21 +10,49 @@ let scissors = new Object("scissors", "paper", "rock");
 let object;
 let AIobjectId;
 let enemyObject;
+let points = 0;
+let AIpoints = 0;
 
 let pencilCase = [rock, paper, scissors];
 function pick(id)
 {
     numberId = parseInt(id);
     object = pencilCase[numberId].name;
-    console.log(object);
+    let choosing = document.getElementById("choosing");
+    choosing.innerText = "You chose " + object + ".";
 }
 function enemyTurn()
 {
     ourAmazingRandomizerAI();
     enemyObject = pencilCase[AIobjectId].name;
-    console.log(enemyObject);
+    compare();
 }
 function ourAmazingRandomizerAI()
 {
     AIobjectId = Math.floor(Math.random() * 3);
+}
+function compare()
+{
+    let tekst = document.getElementById("tekst");
+    let score = document.getElementById("score");
+    if(object == enemyObject)
+    {
+        tekst.innerText = "AI chose " + enemyObject + ". Draw.";
+    }
+    else if(object == pencilCase[AIobjectId].weakness)
+    {
+        tekst.innerText = "AI chose " + enemyObject + ". You win.";
+        points++;
+        score.innerText = "Your points - " + points + ". AI's points - " + AIpoints + ".";
+    }
+    else if(object == pencilCase[AIobjectId].strength)
+    {
+        tekst.innerText = "AI chose " + enemyObject + ". You lose.";
+        AIpoints++;
+        score.innerText = "Your points - " + points + ". AI's points - " + AIpoints + ".";
+    }
+    else
+    {
+        tekst.innerText = "Jakim cudem? Jak tego dokonales? Dalismy ci prosta gre a tobie udalo sie ja zepsuc. Mam nadzieje ze jestes z siebie dumny.";
+    }
 }
